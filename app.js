6f748e598b0bcb4a177169489c276693732df3ad
Minh Trung <nmtrun1001@gmail.com>
2022-08-27 14:50:33 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const contactsRouter = require("./app/routes/contact.routes");
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcom to contact book application." });
 });
+
 module.exports = app;
